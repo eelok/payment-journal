fe168797730f4e6b86452595ef39d8cac87fcd1c
petrius <maria.petrius@gmail.com>
2025-08-06 18:06:00 +0200

    add cors support

---
+++ src/main/java/com/petrius/payment_journal/config/WebConfig.java
@@ -0,0 +1,22 @@
+package com.petrius.payment_journal.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        WebMvcConfigurer.super.addCorsMappings(registry);
+
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:4200")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
+
